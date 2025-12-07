7faa283720886d09809b083a06af4df712a77068
heppocogne <83043568+heppocogne@users.noreply.github.com>
2025-12-08 06:28:09 +0900

    (wip)Update main_view

--- lib/widget/main_view.dart
+++ lib/widget/main_view.dart
@@ -2,18 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:nagur/l10n/app_localizations.dart';
-//import 'package:nagur/model/memo.dart';
+import 'package:nagur/model/memo.dart';
+import 'package:nagur/model/system.dart';
 
 class MainView extends ConsumerWidget {
   const MainView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    String title = L10n.of(context)!.untitled;
+    String content = '';
+    ref.watch(systemProvider).whenData((system) {
+      ref.watch(memoProvider(system.currentMemoUuid)).whenData((memo) {
+        if (memo.title != null) {
+          title = memo.title!;
+        }
+        if (memo.content != null) {
+          content = memo.content!;
+        }
+      });
+    });
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
         centerTitle: true,
-        title: TextFormField(initialValue: L10n.of(context)!.untitled),
+        title: TextFormField(initialValue: title),
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.arrow_downward)),
           IconButton(onPressed: () {}, icon: Icon(Icons.star)),
@@ -24,7 +38,19 @@ class MainView extends ConsumerWidget {
         onPressed: () {},
         child: Icon(Icons.add),
       ),
-      body: null,
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        constraints: BoxConstraints.expand(),
+        child: SingleChildScrollView(
+          child: TextFormField(
+            decoration: InputDecoration(border: InputBorder.none),
+            maxLines: null,
+            autofocus: true,
+            keyboardType: TextInputType.multiline,
+            initialValue: content,
+          ),
+        ),
+      ),
     );
   }
 }
